eb4fd1c1eb5832babc6bfe743a129479efae7ede
Niccolo Dejito <ncdejito@gmail.com>
2022-08-17 13:45:29 +0800

    Replace destination to: from and to

--- tests/test_zulip_comms.py
+++ tests/test_zulip_comms.py
@@ -6,20 +6,20 @@ from src.zulip_comms import get_client, get_posts, send
 client = get_client()
 
 
-@pytest.mark.skip(reason="")
+# @pytest.mark.skip(reason="")
 def test_get_posts():
-    results = get_posts(num_before=1, client=client)
+    results = get_posts(num_before=1, client=client, _from="staging")
     assert len(results) > 0
 
 
-@pytest.mark.skip(reason="")
+# @pytest.mark.skip(reason="")
 def test_send():
-    send("test send", client)
+    send("test send", client, to="staging")
 
-    results = get_posts(num_before=1, client=client)
+    results = get_posts(num_before=1, client=client, _from="staging")
     recs = []
     for msg in results["messages"]:
-        html = BeautifulSoup(msg["content"])
+        html = BeautifulSoup(msg["content"], "html.parser")
         text = html.get_text()
         recs.append(text)
 
